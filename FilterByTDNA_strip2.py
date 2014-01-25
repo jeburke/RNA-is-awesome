1b5168f28b41575073c1ccf794449ca4e68d70eb
Tim Burke <tim.burke@gmail.com>
2014-01-24 23:07:50 -0800

    Tweak match-checking logic

--- FilterByTDNA_strip2.py
+++ FilterByTDNA_strip2.py
@@ -10,27 +10,29 @@ fin = open(sys.argv[1], "r")
 fout = open("{0}_out_strip6.fastq".format(sys.argv[1].split(".")[0]), "w")
 
 TDNA = "TGTCTAAGCGTCAATTTGTTTACACCACAATATATC"
+T_min = 5
 
 left = ""
 headerline = ""
+
 for k, line in enumerate(fin):
-    n = len(TDNA)
     if k % 4 == 1:
         left = 0
-        while line[0:n] == TDNA:
+        for i in range(len(TDNA)):
+            if line[i] != TDNA[i]:
+                left = i
+                break
+        if left >= T_min:
             match = True
-            line = line[n:]
-            left = n
             fout.write(headerline)
-            fout.write(line)
-            n = n - 1
-            TDNA = TDNA[0:n]
+            fout.write(line[left:])
     elif k%4 == 2 and match:
         fout.write(line)
     elif k%4 == 3 and match:
         fout.write(line[left:])
     elif k%4 == 0:
         match = False
+        # cache for later, in case it *is* a match
         headerline = line
 
 
