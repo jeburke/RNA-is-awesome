e96dde02cde75acee6b63a36ba1a9369e4c70817
Tim Burke <tim.burke@gmail.com>
2014-01-24 22:56:03 -0800

    Remove unnecessary variable; don't bother tweaking end-of-line

--- FilterByTDNA_strip2.py
+++ FilterByTDNA_strip2.py
@@ -10,20 +10,16 @@ fin = open(sys.argv[1], "r")
 fout = open("{0}_out_strip6.fastq".format(sys.argv[1].split(".")[0]), "w")
 
 left = ""
-right = ""
 headerline = ""
 for k, line in enumerate(fin):
     n = 36
     TDNA = "TGTCTAAGCGTCAATTTGTTTACACCACAATATATC"
     if k % 4 == 1:
         left = 0
-        right = len(line)
-        line = line.rstrip()
         while line[0:n] == TDNA:
             out = "yes"
             line = line[n:]
             left = n
-            line = line+"\n"
             fout.write(headerline)
             fout.write(line)
             n = n - 1
@@ -31,9 +27,7 @@ for k, line in enumerate(fin):
     elif k%4 == 2 and out == "yes":
         fout.write(line)
     elif k%4 == 3 and out == "yes":
-        line = line.rstrip()
-        line = line[left:]+"\n"
-        fout.write(line)
+        fout.write(line[left:])
     elif k%4 == 0:
         out = "no"
         headerline = line
