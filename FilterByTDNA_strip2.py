fd4937a9398424c0a77d970f463d19c0f12633a1
Tim Burke <tim.burke@gmail.com>
2014-01-24 23:03:16 -0800

    Use a boolean flag instead of a string; avoid numeric constant

--- FilterByTDNA_strip2.py
+++ FilterByTDNA_strip2.py
@@ -9,27 +9,28 @@ import sys
 fin = open(sys.argv[1], "r")
 fout = open("{0}_out_strip6.fastq".format(sys.argv[1].split(".")[0]), "w")
 
+TDNA = "TGTCTAAGCGTCAATTTGTTTACACCACAATATATC"
+
 left = ""
 headerline = ""
 for k, line in enumerate(fin):
-    n = 36
-    TDNA = "TGTCTAAGCGTCAATTTGTTTACACCACAATATATC"
+    n = len(TDNA)
     if k % 4 == 1:
         left = 0
         while line[0:n] == TDNA:
-            out = "yes"
+            match = True
             line = line[n:]
             left = n
             fout.write(headerline)
             fout.write(line)
             n = n - 1
             TDNA = TDNA[0:n]
-    elif k%4 == 2 and out == "yes":
+    elif k%4 == 2 and match:
         fout.write(line)
-    elif k%4 == 3 and out == "yes":
+    elif k%4 == 3 and match:
         fout.write(line[left:])
     elif k%4 == 0:
-        out = "no"
+        match = False
         headerline = line
 
 
