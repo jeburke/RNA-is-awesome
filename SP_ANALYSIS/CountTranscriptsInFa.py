d9a9696e767467cec24d8eeb989b57f506bf4193
jeburke <custom85@gmail.com>
2015-12-14 13:24:10 -0800

    Fixing count transcripts in fasta so it outputs sorted lists

--- SP_ANALYSIS/CountTranscriptsInFa.py
+++ SP_ANALYSIS/CountTranscriptsInFa.py
@@ -9,25 +9,30 @@ fin = open(sys.argv[1], "r")
 fout = open("{0}_tally.fasta".format(sys.argv[1].split(".")[0]), "w")
 
 transcript_list = []
-for j, line in enumerate(fin):
-    if j % 2 == 0:
+for line in fin:
+    if line.startswith(">"):
         transcript_list.append(str(line.split(";")[0]).strip(">"))
-    print transcript_list
+print transcript_list[20]
 
 counter = collections.Counter(transcript_list)
-counter.most_common()
-print counter
+counter_sorted = counter.most_common()
+#print counter
 
 final_transcript_list = []
 transcript_count = []
-for transcript, count in counter.iteritems():
-    final_transcript_list.append(transcript)
-    transcript_count.append(count)
 
-while n < len(transcript_count):
-    line_list = [final_transcript_list[n],"\t",transcript_count[n],"\n"]
+a=0
+while a < len(counter_sorted):
+    final_transcript_list.append(counter_sorted[a][0])
+    transcript_count.append(counter_sorted[a][1])
+    a += 1
+
+b = 0
+while b < len(transcript_count):
+    line_list = [str(final_transcript_list[b]),"\t",str(transcript_count[b]),"\n"]
     line = "".join(line_list)
     fout.write(line)
+    b += 1
 
 fin.close()
 fout.close()
